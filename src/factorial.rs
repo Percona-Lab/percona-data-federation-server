2c917dbb24371af29d0329afa8b84101c5de463a
Konstantin Trushin <konstantin.trushin@percona.com>
2025-08-07 13:28:27 +0400

    PSMDB-1761 Check formatting and run tests

---
+++ src/factorial.rs
@@ -0,0 +1,29 @@
+pub fn compute(n: u128) -> u128 {
+    (1..n + 1).product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_absolute_values() {
+        assert_eq!(compute(0), 1);
+        assert_eq!(compute(1), 1);
+        assert_eq!(compute(2), 2);
+        assert_eq!(compute(3), 6);
+        assert_eq!(compute(4), 24);
+        assert_eq!(compute(5), 120);
+        assert_eq!(compute(6), 720);
+        assert_eq!(compute(7), 5040);
+        assert_eq!(compute(8), 40320);
+        assert_eq!(compute(9), 362880);
+    }
+
+    #[test]
+    fn factorial_value_relationships() {
+        assert_eq!(compute(4) * 5, compute(5));
+        assert_eq!(compute(16) * 17, compute(17));
+        assert_eq!(compute(33) * 34, compute(34));
+    }
+}
---
+++ src/fibonacci.rs
@@ -0,0 +1,39 @@
+pub fn compute(n: u128) -> u128 {
+    if n < 2 {
+        return n;
+    }
+    let mut prev_prev: u128 = 0;
+    let mut prev: u128 = 1;
+    let mut cur = prev_prev + prev;
+    for _ in 3..n + 1 {
+        prev_prev = prev;
+        prev = cur;
+        cur = prev_prev + prev;
+    }
+    cur
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fibonacci_absolute_values() {
+        assert_eq!(compute(0), 0);
+        assert_eq!(compute(1), 1);
+        assert_eq!(compute(2), 1);
+        assert_eq!(compute(3), 2);
+        assert_eq!(compute(4), 3);
+        assert_eq!(compute(5), 5);
+        assert_eq!(compute(6), 8);
+        assert_eq!(compute(7), 13);
+        assert_eq!(compute(8), 21);
+        assert_eq!(compute(9), 34);
+    }
+
+    #[test]
+    fn fibonacci_value_relationships() {
+        assert_eq!(compute(3) + compute(4), compute(5));
+        assert_eq!(compute(126) + compute(127), compute(128));
+    }
+}
