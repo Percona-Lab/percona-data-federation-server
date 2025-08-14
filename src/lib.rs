0e36ca906390ca414323dd360858818161ef731a
Konstantin Trushin <konstantin.trushin@percona.com>
2025-08-14 12:38:02 +0400

    Implement minimal binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+use serde_json::json;
+
 pub mod factorial;
 pub mod fibonacci;
+
+pub fn compute(n1: u128, n2: u128) -> String {
+    let val = json!({"value": factorial::compute(n1) + fibonacci::compute(n2)});
+    val.to_string()
+}
